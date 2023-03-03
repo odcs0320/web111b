b83dd0718bbf1c6a9fad3892cb1056dbb2ace727
JimCheng0310 <odcs0320@msn.com>
2023-03-03 11:52:29 +0800

    0303 上午

--- crud/php_mysqli_read_2.php
+++ crud/php_mysqli_read_2.php
@@ -33,98 +33,30 @@
 
     <!-- 資料內容 -->
     <?php
-    require_once("connDB.php");
-
-    $sql = "SELECT * FROM `students`";
-    $result = mysqli_query($conn, $sql);
-
-    while ($row = mysqli_fetch_assoc($result)) { ?>
-
-    <tr align="center">
-      <td> <?php echo  $row['cID']; ?> </td>
-      <td> <?php echo  $row['cName']; ?> </td>
-      <td> <?php echo  $row['cSex']; ?> </td>
-      <td> <?php echo  $row['cBirthday']; ?> </td>
-      <td> <?php echo  $row['cEmail']; ?> </td>
-      <td> <?php echo  $row['cPhone']; ?> </td>
-      <td> <?php echo  $row['cAddr']; ?> </td>
-      <td> <?php echo  $row['cHeight']; ?> </td>
-      <td> <?=  $row['cWeight']; ?></td>
-    </tr>
-
-
-
-
-
-    }
-
-    ?>
-
-
-
-
-
-
-
-
-  </table>
-  </p>
-
-
-</body>
-
-</html>
-
-
-
-
-
-<?php
-
-require_once("connDB.php");
+require_once "connDB.php";
 
 $sql = "SELECT * FROM `students`";
 $result = mysqli_query($conn, $sql);
-// $row = mysqli_fetch_assoc($result);
 
-//資料內容呈現
-echo "<p><table align=center border=1>";
-//資料表頭
-echo "<tr>
-    <th>座號</th>
-    <th>姓名</th>
-    <th>性別</th>
-    <th>生日</th>
-    <th>電子郵件</th>
-    <th>電話</th>
-    <th>住址</th>
-    <th>身高</th>
-    <th>體重</th>
-</tr>";
+while ($row = mysqli_fetch_assoc($result)) {?>
 
-//資料內容
-while ($row = mysqli_fetch_assoc($result)) {
-  <tr align='center'>
-  <td> <?php echo $row['cID']; ?> </td>
-<td> <?php echo $row['cName']; ?> </td>
-<td> <?php echo $row['cSex']; ?> </td>
-<td> <?php echo $row['cBirthday']; ?> </td>
-<td> <?php echo $row['cEmail']; ?> </td>
-<td> <?php echo $row['cPhone']; ?> </td>
-<td> <?php echo $row['cAddr']; ?> </td>
-<td> <?php echo $row['cHeight']; ?> </td>
-<td> <?php echo $row['cWeight']; ?> </td>
-
-</tr><?php
+    <tr align="center">
+      <td> <?php echo $row['cID']; ?> </td>
+      <td> <?php echo $row['cName']; ?> </td>
+      <td> <?php echo $row['cSex']; ?> </td>
+      <td> <?php echo $row['cBirthday']; ?> </td>
+      <td> <?php echo $row['cEmail']; ?> </td>
+      <td> <?php echo $row['cPhone']; ?> </td>
+      <td> <?php echo $row['cAddr']; ?> </td>
+      <td> <?php echo $row['cHeight']; ?> </td>
+      <td> <?=$row['cWeight'];?></td>
+    </tr><?php
 }
-
 mysqli_free_result($result);
 mysqli_close($conn); //關閉資料庫連接
 ?>
-
-
-</table>
-</p>
+  </table>
+  </p>
 </body>
 
 </html>
